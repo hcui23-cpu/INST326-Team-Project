5b70c907e3877f7e3598f3fc226d601c9abbeeb0
nurbaez-bit <nurbaez@terpmail.umd.edu>
2025-10-12 13:29:32 -0400

    Update __init__.py

--- SRC/__init__.py
+++ SRC/__init__.py
@@ -1,1 +1,11 @@
+"""
+Financial Management Function Library
+
+A comprehensive function library for money tracking, categorization, 
+and transaction cleaning.
+
+Author: Nathan Urbaez and 
+Course: Object-Oriented Programming for Information Science
+Institution: University of Maryland, College Park
+"""
 
